1a6b10edb2db5ea0e0adf80430d567999c4298b9
JaynAp1 <jerxonmjair@gmail.com>
2025-03-16 20:39:45 -0600

    :sparkles: Crud day20 finalized

--- dia20/src/CrudTXT.java
+++ dia20/src/CrudTXT.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CrudTXT {
-    private static String archivo = "C:\\Users\\Uniminuto Tibu\\IdeaProjects\\dia20\\src\\data.txt";
+    private static String archivo = "C:\\Users\\Correa\\Documents\\Java_CorreaJerxon\\dia20\\src\\data.txt";
     private static List<Persona> personas = new ArrayList<>();
     private static List<Producto> productos = new ArrayList<>();
 
@@ -33,6 +33,9 @@ public class CrudTXT {
             for (Persona p : personas) {
                 writer.write(p.toString() + "\n");
             }
+            for (Producto p : productos) {
+                writer.write(p.toString() + "\n");
+            }
         } catch (IOException e) {
             System.err.println("Error al guardar datos: " + e.getMessage());
         }
@@ -40,9 +43,48 @@ public class CrudTXT {
     public static void agregarPersona(int id, String nombre, int edad) {
         personas.add(new Persona(id, nombre, edad));
         guardarDatos();
+        System.out.println("Agregado con exito");
     }
 
+    public static void agregarProducto(int id, String nombre, double precio){
+        productos.add(new Producto(id, nombre, precio));
+        guardarDatos();
+        System.out.println("Agregado con exito");
+    }
+    public static void actualizarPersona(int id, String nuevoNombre, int nuevaEdad) {
+        for (Persona p : personas) {
+            if (p.getId() == id) {
+                p.setNombre(nuevoNombre);
+                p.setEdad(nuevaEdad);
+                guardarDatos();
+                return;
+            }
+        }
+        System.out.println("Persona con ID " + id + " no encontrada.");
+    }
+    public static void actualizarProducto(int id, String nombre, double precio) {
+        for (Producto p : productos) {
+            if (p.getId() == id) {
+                p.setNombre(nombre);
+                p.setPrecio(precio);
+                guardarDatos();
+                return;
+            }
+        }
+    }
 
+    public static void eliminarPersona(int id) {
+        personas.removeIf(p -> p.getId() == id);
+        guardarDatos();
+        System.out.println("Eliminado con exito");
+    }
+
+    public static void eliminarProducto(int id) {
+        productos.removeIf(p -> p.getId() == id);
+        guardarDatos();
+        System.out.println("Eliminado con exito");
+
+    }
 
     public static void listarPersonas(){
         if (personas.isEmpty()){
@@ -54,7 +96,7 @@ public class CrudTXT {
         }
     }
     public static void listarProducto(){
-        if (personas.isEmpty()){
+        if (productos.isEmpty()){
             System.out.println("No se puede listar productos por que no hay");
         }else{
             for (Producto producto: productos){
--- dia20/src/Main.java
+++ dia20/src/Main.java
@@ -1,12 +1,144 @@
-
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         CrudTXT.cargarDatos();
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        boolean boleanito = true;
+        while(boleanito) {
+            System.out.println("""
+                    =======================================
+                    1).Ver datos.
+                    2).Eliminar datos.
+                    3).Cargar datos.
+                    4).Actualizar datos.
+                    5).Salir.
+                    """);
 
-        CrudTXT.listarProducto();
-        CrudTXT.agregarPersona(10,"john",40);
+            String Seleccion = sc.nextLine();
+            switch(Seleccion) {
+                case "1"->{
+                    System.out.println("""
+                            =========================
+                            1).Ver objetos.
+                            2).Ver personas.
+                            """);
+                    String Seleccion2 = sc.nextLine();
+                    switch(Seleccion2) {
+                        case "1"->{
+                            CrudTXT.listarProducto();
+                        }
+                        case "2"->{
+                            CrudTXT.listarPersonas();
+                        }
+                    }
+                }
+                case "2"->{
+                    System.out.println("""
+                            =========================
+                            1).Eliminar objetos.
+                            2).Elimiar personas.
+                            """);
+                    String Seleccion2 = sc.nextLine();
 
-        CrudTXT.listarPersonas();
+                    switch (Seleccion2){
+                        case "1"->{
+                            System.out.println("Ingrese el id del producto que desea eliminar");
+                            int id = sc.nextInt();
+                            sc.nextLine();
+                            CrudTXT.eliminarProducto(id);
+
+                        }
+                        case "2"->{
+                            System.out.println("Ingrese el id de la persona que desea eliminar");
+                            int id = sc.nextInt();
+                            sc.nextLine();
+                            CrudTXT.eliminarPersona(id);
+                        }
+                    }
+
+                }
+                case "3"->{
+                    System.out.println("""
+                            =========================
+                            1).Agregar objetos.
+                            2).Agregar personas.
+                            =========================
+                            """);
+                    String Seleccion2 = sc.nextLine();
+                    switch (Seleccion2){
+                        case "1"->{
+                            System.out.println("Ingrese el id del objeto que desea agregar: ");
+                            int id = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("Ingrese el nombre del objeto que desea agregar: ");
+                            String Nombre = sc.nextLine();
+                            System.out.println("Ingrese el precio del objeto que desea agregar: ");
+                            double Precio = sc.nextDouble();
+                            sc.nextLine();
+
+                            CrudTXT.agregarProducto(id, Nombre, Precio);
+                        }
+                        case "2"->{
+                            System.out.println("Ingrese el id de la persona que desea agregar: ");
+                            int id = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("Ingrese el nombre de la persona que desea agregar: ");
+                            String Nombre = sc.nextLine();
+                            System.out.println("Ingrese la edad de la persona que desea agregar: ");
+                            int edad = sc.nextInt();
+                            sc.nextLine();
+
+                            CrudTXT.agregarPersona(id, Nombre, edad);
+                        }
+                    }
+                }
+                case "4"->{
+                    System.out.println("""
+                            =========================
+                            1).Actualizar objetos.
+                            2).Actualizar personas.
+                            =========================
+                            """);
+                    String Seleccion2 = sc.nextLine();
+                    switch (Seleccion2){
+                        case "1"->{
+                            System.out.println("Ingrese el id del objeto que desea actualizar: ");
+                            int id = sc.nextInt();
+                            sc.nextLine();
+
+                            System.out.println("Ingrese el nuevo nombre del producto que desea actualizar: ");
+                            String nombre = sc.nextLine();
+                            System.out.println("Ingrese el nuevo precio del producto: ");
+                            double precio = sc.nextDouble();
+                            sc.nextLine();
+
+                            CrudTXT.actualizarProducto(id, nombre, precio);
+                        }
+                        case "2"->{
+                            System.out.println("Ingrese el id de la persona que desea actualizar: ");
+                            int id = sc.nextInt();
+                            sc.nextLine();
+
+                            System.out.println("Ingrese el nuevo nombre de la persona que desea actualizar: ");
+                            String nombre = sc.nextLine();
+                            System.out.println("Ingrese la nueva edad de la persona que desea actualizar: ");
+                            int edad = sc.nextInt();
+                            sc.nextLine();
+
+                            CrudTXT.actualizarPersona(id, nombre, edad);
+                        }
+                    }
+                }
+                case "5"->{
+                    boleanito = false;
+                }
+                default -> {
+
+                }
+            }
+        }
     }
 }
